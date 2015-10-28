08b9744a34bc2b3d5637cfc5f5ab93a31d79771c
Alessandro Manzotti <manzotti.alessandro@gmail.com>
2015-10-28 17:38:01 -0500

    change plot label

--- n_priors_code/plotting/plot_prior_paper_M_nu_ell4.py
+++ n_priors_code/plotting/plot_prior_paper_M_nu_ell4.py
@@ -229,7 +229,7 @@ for key_y in ['omnuh2']:
     ax1.set_ylim((0.8 * np.amin(new_sigma_all)*94. * 1000., 1.1 * np.amax(new_sigma)*94. * 1000.))
     ax1.set_xlim((0.1 , 3.1))
     # ax1.set_title(r'$\sigma({0})={1:.1f}\%$'.format(str(label[key_y]), np.abs(sigma_just_CMB_y / fid[key_y] * 100.)))
-    ax1.set_ylabel(r'$\sigma(M_{\nu}) $ meV')
+    ax1.set_ylabel(r'$\sigma(\sum m_\nu) $ meV')
     ax1.set_xlabel(r'$\rm{prior}/\sigma(x)_{\rm CMB}$')
 
     y1, y2 = ax1.get_ylim()
